5530b6c8b5c9bc063109e4846b5e296ab68a0027
VidyaSagarMehar <vidyasagark890@gmail.com>
2022-12-05 22:53:42 +0530

    user schema added

--- models/user.schema.js
+++ models/user.schema.js
@@ -1,9 +1,35 @@
 import mongoose from 'mongoose';
+import AuthRoles from '../utils/authRoles';
 
-const userSchema = mongoose.Schema({
-	name: {
-		type: String,
-		required: [true, 'Name is required'],
-		maxLength: [50, 'Name must be less than 50 character'],
+const userSchema = mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: [true, 'Name is required'],
+			maxLength: [50, 'Name must be less than 50 character'],
+		},
+		email: {
+			type: String,
+			required: [true, 'Email is required'],
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: [true, 'Password is required'],
+			minLength: [8, 'Password must be at least 8 character'],
+			select: false,
+		},
+		role: {
+			type: String,
+			enum: Object.values(AuthRoles),
+			default: AuthRoles.USER,
+		},
+		forgotPasswordToken: String,
+		forgotPasswordExpiry: Date,
 	},
-});
+	{
+		timestamps: true, // It has two properties ie; createdAt, updateAt
+	},
+);
+
+export default mongoose.model('User', userSchema);
